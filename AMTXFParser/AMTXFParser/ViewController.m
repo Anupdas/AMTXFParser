3b4b208ca6e0b3684e7b503991f29306ed11f290
Anoop Mohandas <anoopmdas@gmail.com>
2015-02-01 09:29:38 +0530

    Added parsing controls from UI

--- AMTXFParser/AMTXFParser/ViewController.m
+++ AMTXFParser/AMTXFParser/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "NSString+TXFParser.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (NSString *)txfStringForName:(NSString *)fileName{
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:fileName
+                                                         ofType:@"txf"];
+    return  [[NSString alloc] initWithContentsOfFile:filePath
+                                            encoding:NSUTF8StringEncoding
+                                               error:nil];
+}
+
+- (IBAction)parseButtonClick:(UIButton *)sender {
+    NSString *txfString = [self txfStringForName:nil];
+    id object = [txfString TXFObject];
+    
 }
 
 - (void)didReceiveMemoryWarning {
