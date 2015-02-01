0c7f3d62d5e097a213398a396a1eb6c89e17e4a4
Anoop Mohandas <anoopmdas@gmail.com>
2015-02-01 09:22:09 +0530

    Initial Commit

---
+++ AMTXFParser/AMTXFParser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMTXFParser
+//
+//  Created by Anoop Mohandas on 01/02/15.
+//  Copyright (c) 2015 Anoop Mohandas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AMTXFParser/AMTXFParser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMTXFParser
+//
+//  Created by Anoop Mohandas on 01/02/15.
+//  Copyright (c) 2015 Anoop Mohandas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
